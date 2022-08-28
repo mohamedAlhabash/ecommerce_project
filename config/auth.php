a6d6099d580616e31a01960faf7161a5c47b14df
mohamedAlhabash <mohamedwaha2000@gmail.com>
2022-08-28 12:00:04 +0300

    first commit

--- config/auth.php
+++ config/auth.php
@@ -62,7 +62,8 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => Namespace\Of\Your\User\Model\User::class,
+            'table' => 'users',
         ],
 
         // 'users' => [
